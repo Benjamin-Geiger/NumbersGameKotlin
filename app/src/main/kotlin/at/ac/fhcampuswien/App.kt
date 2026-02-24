3a61fd6560858e7d528dc9f6d04d2adcd9ef014e
Benjamin Geiger <benjamin.geiger@stud.fh-campuswien.ac.at>
2026-02-24 11:16:44 +0100

    Tasks 1-3

--- app/src/main/kotlin/at/ac/fhcampuswien/App.kt
+++ app/src/main/kotlin/at/ac/fhcampuswien/App.kt
@@ -4,4 +4,7 @@ fun main() {
     println("Hello Kotlin!")
     // Start the game
     // user GameManager.start()
+    var Game = GameLogic
+    val num = Game.generateRandomNumber(4)
+    println(num)
 }
--- app/src/main/kotlin/at/ac/fhcampuswien/GameLogic.kt
+++ app/src/main/kotlin/at/ac/fhcampuswien/GameLogic.kt
@@ -2,6 +2,30 @@ package at.ac.fhcampuswien
 
 import at.ac.fhcampuswien.GuessResult
 
-class GameLogic() {
+class GameLogic(val initialDigitsToGuess: Int = DEFAULT_DIGITS) {
+    var digitsToGuess: Int = initialDigitsToGuess
+        private set
 
+    val targetNumber: List<Int> = generateRandomNumber(DEFAULT_DIGITS)
+
+
+    companion object Numbers{
+        val DEFAULT_DIGITS = 4
+
+        fun generateRandomNumber(digits: Int): List<Int>{
+            val numbers: MutableList<Int> = mutableListOf()
+            for(i in 1..9){
+                numbers.add(i)
+            }
+            val numbersShuffled: List<Int> = numbers.shuffled()
+
+            val numbersLocked: MutableList<Int> = mutableListOf()
+
+            for(i in 1..digits){
+                numbersLocked.add(numbersShuffled[i])
+            }
+            return numbersLocked
+
+        }
+    }
 }
